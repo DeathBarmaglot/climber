5eff5aed3fc3992eb08c0bec1a513310206388f9
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2021-01-28 16:40:17 +0200

    Update index_edgePreload.js

--- new/index_edgePreload.js
+++ new/index_edgePreload.js
@@ -51,8 +51,8 @@ if(document.addEventListener ){
 
    aLoader = [
       
-      { load: "https://raw.githack.com/DeathBarmaglot/Vik.io/master/edge_includes/edge_includes/jquery-2.0.3.min.js"},
-      { load: "https://raw.githack.com/DeathBarmaglot/Vik.io/master/edge_includes/edge_includes/edge.4.0.0.min.js"},
+      { load: "https://raw.githack.com/DeathBarmaglot/Vik.io/master/edge_includes/edge.4.0.0.min.js"},
+      { load: "https://raw.githack.com/DeathBarmaglot/Vik.io/master/edge_includes/edge.4.0.0.min.js"},
       { load: "index_edge.js"},
       { load: "index_edgeActions.js"}];
 
